9a3c337df1adf8f9c0defb39b6948694583e5a10
Hadil24A <hadil.ahmed@hva.nl>
2024-06-04 23:08:13 +0200

    Project opgezet
    - Node
    - Express
    - EJS
    - CSS custom properties

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-console.log('Hier komt je server voor Sprint 12.')
+// 1. OPZETTEN VAN DE WEBSERVER
+
+import express, { request } from "express"
+
+import fetchJson from "./helpers/fetch-json.js"
+
+let app = express()
+
+app.set("view engine", "ejs")
+
+app.set("views", "./views")
+
+app.use(express.static("public"))
+
+app.use(express.urlencoded({ extended: true }))
+
+// 2. ROUTES
+
+app.get("/", function (request, response) {
+  fetchJson('https://fdnd-agency.directus.app/items/DPI_TNO')
+    .then(function(introData) {
+      response.render('index', {
+        introData: introData
+      });
+    });
+});
+
+// 3. START DE WEBSERVER
+
+app.set("port", process.env.PORT || 8000)
+app.listen(app.get("port"), function () {
+  console.log(`Application started on http://localhost:${app.get("port")}`)
+})
