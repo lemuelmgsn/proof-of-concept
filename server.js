3a653e93ed59b130fdb30f20c4bc6107702e473c
lemuelmgsn <magsinolemuel68@gmail.com>
2024-06-05 12:21:03 +0200

    opstarten project
    klein begin gemaakt met het opstarten van project

--- server.js
+++ server.js
@@ -10,18 +10,15 @@ import fetchJson from './helpers/fetch-json.js'
 // const squadData = await fetchJson('https://fdnd.directus.app/items/squad')
 // Ik ga dit misschien later nog gebruiken
 
+
 // const apiUrl = 'https:fdnd-agency.directus.app/items/f_houses' moet nog veranderen naar nieuwe url met API
-const apiUrl = 'https:fdnd-agency.directus.app/items/'
+const apiUrl = 'https://fdnd-agency.directus.app/items/DPI_TNO?fields=*'
 
 
-// notitie van rating pagina 
-let ratings = []
 
 
 // Maak een nieuwe express app aan
 const app = express()
-
-
 
 // Stel ejs in als template engine
 app.set('view engine', 'ejs')
@@ -35,120 +32,21 @@ app.use(express.static('public'))
 // Zorg dat werken met request data makkelijker wordt
 app.use(express.urlencoded({extended: true}))
 
+
+
 // ROUTES
 
 app.get('/', function(request, response) {
-  fetchJson(apiUrl + 'f_list').then((apiData) => {
-    response.render('index', {data: apiData.data})
-  });
-})
-
-
-
-app.get('/favorieten', function(request, response) {
-  fetchJson(apiUrl+ 'f_list').then((lists) => {
-    fetchJson(apiUrl + 'f_users').then((users) => {
-      response.render('favorieten', {
-      lists: lists.data,
-      users: users.data
-      })
-    });
-	});
-})
-
-app.get('/lijst/:id', function(request, response) {
-    fetchJson(apiUrl + 'f_users').then((users) => {
-      fetchJson(apiUrl + 'f_list/' + request.params.id + '?fields=*.*.*,houses.f_houses_id.poster_image.id,houses.f_houses_id.poster_image.width,houses.f_houses_id.poster_image.height').then((lists) => { 
-      response.render('fav-lijst.ejs', {
-        list: lists.data,
-        users: users.data
-      })  
-     })
-    });
-	});
-
-app.get('/users/:id', function(request, response) {
-  fetchJson(apiUrl + 'f_houses').then((houses) => {
-    fetchJson(apiUrl + 'f_users').then((users) => {
-      response.render('users', {
-      houses: houses.data,
-      users: users.data
-      })
-    });
-	});
-})
-
-
-app.get('/detail/:id', function(request, response) {
-  fetchJson(apiUrl + 'f_houses/' + request.params.id).then((apiData) => {
-      // console.log(apiData)
-      response.render('details', {
-        data: apiData.data})
-  });
-})
-
-app.get('/user-ratings', function(request, response) {
-  fetchJson(apiUrl + 'f_houses').then((apiData) => {
-      response.render('user-ratings', {
-        data: apiData.data
-      })
-  });
-})
-
-// POST TEST BEGIN
-
-
-app.get('/rating-maken', function (request, response) {
-  // fetch data directus table f_feedback
-  fetchJson(apiUrl + 'f_feedback').then((BeoordelingData) => {
-    console.log(BeoordelingData)
-
-    response.render('rating-maken', {
-      alleHuizen: huizenHome.data,
-      alleRatings: feedback.data,
-      ratings: ratings,
+  fetchJson(apiUrl).then((apiData) => {
+    response.render('index', {
+      data: apiData.data
     })
-    console.log(ratings)
-  })
-})
-
-app.post('/rating-maken', function (request, response) {
-  console.log(request.body)
-
-  // posten naar directus..
-  fetch(`${apiUrl}f_feedback/`, {
-    method: 'POST',
-    body: JSON.stringify({
-      house: request.body.id,
-      list: 10,
-      user: 2,
-      rating: {
-      stars: request.body.stars,
-      keuken: request.body.stars,
-      tuin: request.body.stars,
-      ligging: request.body.stars,
-      badkamer: request.body.stars,
-      prijs: request.body.stars,
-      oppervlakte: request.body.stars,
-      },
-    }),
-    headers: {
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-  }).then((postResponse) => {
-    console.log(postResponse)
-    response.redirect(303, '/rating-maken')
-  })
-})
-
-
-app.get('/ster-rating-persoon', function(request, response) {
-  fetchJson(apiUrl + 'f_houses').then((apiData) => {
-    response.render('ster-rating-persoon')
   });
 })
 
-// 
+
+
+
 
 // Stel het poortnummer in waar express op moet gaan luisteren
 app.set('port', process.env.PORT || 8000)
