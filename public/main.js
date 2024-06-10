87a3ce7e7798c8aa5906d7ce86620b1c6dd7aa96
Hadil24A <hadil.ahmed@hva.nl>
2024-06-10 10:21:27 +0200

    button (animation)

---
+++ public/main.js
@@ -0,0 +1,15 @@
+// let closeButtons = document.querySelectorAll('.closePopupButton');
+// let popoverButtons = document.querySelectorAll('.popoverButton');
+// let targetId = button.getAttribute('popovertarget');
+// let popover = document.getElementById(targetId);
+
+// document.addEventListener('DOMContentLoaded', function () {
+
+//     closeButtons.forEach(button => {
+//         button.addEventListener('click', function () {
+//             if (popover) {
+//                 popover.classList.add('closedPopup');
+//             }
+//         });
+//     });
+// });
