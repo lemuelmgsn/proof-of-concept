1e7d1ff3572f2b8178ee5eb0223c042d1d3e33da
Hadil24A <hadil.ahmed@hva.nl>
2024-06-10 13:54:08 +0200

    aanpassingen + pop up with js toggle

--- public/main.js
+++ public/main.js
@@ -13,3 +13,22 @@
 //         });
 //     });
 // });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Open popup
+    document.querySelectorAll('.openPopup').forEach(button => {
+        button.addEventListener('click', () => {
+            let popupId = button.getAttribute('data-popup-id');
+            document.getElementById(popupId).classList.add('active');
+        });
+    });
+
+    // Close popup
+    document.querySelectorAll('.closePopupButton').forEach(button => {
+        button.addEventListener('click', () => {
+            button.closest('.mobilePopup').classList.remove('active');
+        });
+    });
+});
