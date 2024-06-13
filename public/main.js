f4bebb67dffd2306e5ba17f8c66e8aa63637b25f
Hadil24A <hadil.ahmed@hva.nl>
2024-06-13 11:52:50 +0200

    tijdelijke styling

--- public/main.js
+++ public/main.js
@@ -1,34 +1,24 @@
-// let closeButtons = document.querySelectorAll('.closePopupButton');
-// let popoverButtons = document.querySelectorAll('.popoverButton');
-// let targetId = button.getAttribute('popovertarget');
-// let popover = document.getElementById(targetId);
-
-// document.addEventListener('DOMContentLoaded', function () {
-
-//     closeButtons.forEach(button => {
-//         button.addEventListener('click', function () {
-//             if (popover) {
-//                 popover.classList.add('closedPopup');
-//             }
-//         });
-//     });
-// });
-
-
 document.addEventListener('DOMContentLoaded', () => {
 
     // Open popup
     document.querySelectorAll('.openPopup').forEach(button => {
         button.addEventListener('click', () => {
-            let popupId = button.getAttribute('data-popup-id');
-            document.getElementById(popupId).classList.add('active');
+            let popupId = button.getAttribute('data-popup-id')
+            document.getElementById(popupId).classList.add('active')
+            button.closest('#infographic').document.classList.add('zoom')
         });
     });
 
     // Close popup
+    
     document.querySelectorAll('.closePopupButton').forEach(button => {
         button.addEventListener('click', () => {
-            button.closest('.mobilePopup').classList.remove('active');
+            button.closest('.mobilePopup').classList.remove('active')
+            button.closest('#infographic').document.classList.remove('zoom')
+
         });
     });
 });
+
+
+// Zoom button
